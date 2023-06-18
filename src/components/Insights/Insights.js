ffc9963216b5762e38ec128d8c87470cf29a9324
Dhananjai Sharma <dhananjaisharma09@gmail.com>
2023-06-18 23:54:00 +0530

    init + navbar

---
+++ src/components/Insights/Insights.js
@@ -0,0 +1,5 @@
+import React from "react";
+import "./Insights.css";
+export const Insights = () => {
+  return <div>Insights</div>;
+};
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Navbar.css";
+
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="content">
+        <h1 className="nav-head">Dashboard</h1>
+      </div>
+    </div>
+  );
+};
